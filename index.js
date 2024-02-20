95193302adf0cddc2c784c0e7ba350fb3d51b458
Shubham Parab <71990977+Skparab1@users.noreply.github.com>
2024-02-20 13:09:45 -0800

    stuff

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+const httpsServer = createServer();
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+const io = new Server(httpsServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500", "http://127.0.0.1:8080", "http://127.0.0.1:4201"]
     }
-    
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
+});
+
+io.on("connection", socket => {
+
+    console.log(`User ${socket.id} connected`)
+
+    socket.on("message", data => {
+
+        console.log(data);
+
+        io.emit("message", `${socket.id.substring(5)}: ${data}`);
     });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+});
+
+httpsServer.listen(3500, () => {
+    console.log("Listening on port 3500");
 });
